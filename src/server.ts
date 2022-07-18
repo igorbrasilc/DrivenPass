88a8719459db7a594499247ea52c9ad5bea5fc70
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-07-17 22:10:29 -0300

    fix heroku

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import AppLog from './events/AppLog.js';
 import app from './app.js';
 import 'dotenv/config';
 
-const PORT = +process.env.PORT || 4000;
-const HOST = process.env.HOST || 'localhost';
+const { PORT, HOST } = process.env;
+// const PORT = +process.env.PORT || 4000;
+// const HOST = process.env.HOST || 'localhost';
 
 app.get('/', async (_req: Request, res: Response) => res.send('Online'));
 app.listen(PORT, HOST, () => AppLog('Server', `Server running on port ${PORT}`));
