3e651ca2b886a07a116e151b9361292359a7c74d
Luana Lessa <lessalsn@gmail.com>
2022-08-01 20:31:10 -0300

    Code review

--- src/schemas/wifiSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-import { WifiInput } from '../interfaces/wifiTypes.js';
-
-export const wifiSchema = joi.object<WifiInput>({
-    title: joi.string().required(),
-    networkName: joi.string().required(),
-    password: joi.string().required(),
-});
